7013944038142abd23cd5d430c68d071e3428383
akrysl <akrysl@calpoly.edu>
2017-06-11 12:27:32 -0700

    added files to study

--- merge_sort.py
+++ merge_sort.py
@@ -1,1 +1,29 @@
+def merge(left, right):
+    print("\nleft:", left, "\nright:", right)
+    result = []
+    i, j = 0, 0
+    while (len(result) < len (left) + len(right)):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+        if i == len(left):
+            result.extend(right[j:])
+        if j == len(right):
+            result.extend(left[i:])
+    print("result", result)
+    return result
 
+def mergesort(list):
+    if (len(list) < 2):
+        return list
+
+    middle = len(list)//2
+    left = mergesort(list[:middle])
+    right = mergesort(list[middle:])
+
+    return merge(left, right)
+
+print ("Final:", mergesort([3,11,4,5,1,2,12,8,3,7,6,9,10]))
--- redblack.py
+++ redblack.py
@@ -1,1 +1,68 @@
+# A FloatRedBlackTree is one of
+# None, or
+# Node(color, RedBlackTree, float, RedBlackTree)
+class Node:
+    def __init__(self, color, left, val, right):
+        self.color = color
+        self.left = left
+        self.val = val
+        self.right = right
 
+    def __eq__(self, other):
+        return ((type(other) == Node)
+          and self.color == other.color
+          and self.left == other.left
+          and self.val == other.val
+          and self.right == other.right
+        )
+
+    def __repr__(self):
+        return ("Node({!r}, {!r}, {!r}, {!r})".format(self.color, self.left, self.val, self.right))
+
+# a color is one of
+# - "R"
+# - "B"
+
+# RedBlackTree val -> RedblackTree
+# Insert the value into the tree
+def insert(t, v):
+    n = insert2(t, v)
+    return Node("B", n.left, n.val, n.right)
+
+# RedBlackTree val -> RedBlackTree
+# Insert the value into the tree
+def insert2(t, v):
+  if t == None:
+    return Node("R", None, v, None)
+  else:
+    if v < t.val:
+      return rebalance(Node(t.color, insert2(t.left, v), t.val, t.right))
+    else:
+      return rebalance(Node(t.color, t.left, t.val, insert2(t.right, v)))
+
+# Node val -> Node
+# rebalance a tree with a chain of two red nodes below a black root
+def rebalance(t):
+  if (t.color == "B" and t.left != None and t.left.color == "R"
+      and t.left.left != None and t.left.left.color == "R"):
+    return Node("R", Node("B", t.left.left.left, t.left.left.val, t.left.left.right),
+                t.left.val,
+                Node("B", t.left.right, t.val, t.right))
+  elif (t.color == "B" and t.left != None and t.left.color == "R"
+      and t.left.right != None and t.left.left.right == "R"):
+    return Node("R", Node("B", t.left.left, t.left.val, t.left.right.left),
+                t.left.right.val,
+                Node("B", t.left.right.right, t.val, t.right))
+  # ... (2 more clauses like this)
+  else:
+    return t
+
+rb = None
+rb= insert(rb, 40)
+print(rb)
+rb= insert(rb, 30)
+print(rb)
+rb= insert(rb, 20)
+print(rb)
+rb= insert(rb, 10)
+print(rb)
